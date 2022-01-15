7cec9a9b03a8d1ccbd804da81716a415dad5c7ca
okirpane <kirpo476@newschool.edu>
2022-01-15 08:13:05 +0000

    checkpoint before polishing

--- src/routes/[slug]/__layout.svelte
+++ src/routes/[slug]/__layout.svelte
@@ -62,24 +62,6 @@
 <svelte:head>
 	<title>{post.title}</title>
 	<meta name="description" content={post.summary} />
-	<!-- <link
-		rel="stylesheet"
-		href="https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.min.css"
-		integrity="sha384-R4558gYOUz8mP9YWpZJjofhk+zx0AS11p36HnD2ZKj/6JR5z27gSSULCNHIRReVs"
-		crossorigin="anonymous"
-	/>
-	<script
-		defer
-		src="https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/katex.min.js"
-		integrity="sha384-z1fJDqw8ZApjGO3/unPWUPsIymfsJmyrDVWC8Tv/a1HeOtGmkwNd/7xUS0Xcnvsx"
-		crossorigin="anonymous"></script>
-	<script
-		defer
-		src="https://cdn.jsdelivr.net/npm/katex@0.15.1/dist/contrib/auto-render.min.js"
-		integrity="sha384-+XBljXPPiv+OzfbB3cVmLHf4hdUFHlWNZN5spNQ7rmHTXpd7WvJum6fIACpNNfIR"
-		crossorigin="anonymous"
-		onload="renderMathInElement(document.body);"></script> -->
-		
 </svelte:head>
 
 <div class="container">
@@ -131,6 +113,7 @@
 		</div>
 		<div class="center-spacer" />
 	</div>
+	<div class="spacer hundred" />
 	<div class="spacer bottom" style={'padding: 15px'} />
 </div>
 
