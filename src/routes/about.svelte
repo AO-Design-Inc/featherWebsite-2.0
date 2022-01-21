457520a0030c5abb38610f90d581d4a3e7b52ba1
okirpane <kirpo476@newschool.edu>
2022-01-21 16:26:32 +0000

    checkpoint: final about layout before deploy

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -26,19 +26,19 @@
 <div id="about-container">
 	<div class="container">
 		<div class="spacer vert one" />
-		<picture class="image">
+		<div class="spacer vert one" />
+		<!-- <picture class="image">
 			<img src="/about-illus.webp" alt="An illustration of what we do summed up abstractly." />
-		</picture>
-		<h1>About Us</h1>
+		</picture> -->
+		<h1 class="text">About Us</h1>
 		<div class="spacer vert two" />
 		<h2 class="text">Who we are</h2>
 		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et sodales elit. Maecenas
-			quis sapien a lacus hendrerit imperdiet. Sed luctus bibendum enim, ut ornare magna sodales
-			non. Sed sed nisl in eros efficitur tristique venenatis sit amet turpis. Vivamus in tortor
-			elit. Aenean lacinia semper orci.
+			We are Feather Systems!<br />
+			So, Computers don’t work well together (especially across platforms). This leads to a lot of computer
+			hardware running tasks that they are not necessarily optimised for. We turn people’s computers
+			into routers for processing power, so that compute can happen where it is best for it to happen.
 		</p>
-		<div class="spacer vert one" />
 		<h2 class="text">What we do</h2>
 		<p>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et sodales elit. Maecenas
@@ -46,7 +46,6 @@
 			non. Sed sed nisl in eros efficitur tristique venenatis sit amet turpis. Vivamus in tortor
 			elit. Aenean lacinia semper orci.
 		</p>
-		<div class="spacer vert one" />
 		<h2 class="text">Where we are going</h2>
 		<p>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus et sodales elit. Maecenas
@@ -54,12 +53,13 @@
 			non. Sed sed nisl in eros efficitur tristique venenatis sit amet turpis. Vivamus in tortor
 			elit. Aenean lacinia semper orci.
 		</p>
+		<h1>Our Team</h1>
 		<div class="spacer vert one" />
-		<h1 class="text">Our Team</h1>
+		<div class="spacer vert one" />
 		<div class="avatar-container">
 			{#each teamStruct as { name, title, picture }, i}
 				<div class="avatar-cards grow">
-					<div class="filter"/>
+					<div class="filter" />
 					<img src={picture} alt={name} />
 					<div class="name">{name}</div>
 					<div class="title">{title}</div>
@@ -86,30 +86,17 @@
 		@media screen and (max-width: $aboutbp) {
 			width: max(get-vw(660px), 90%);
 		}
-		.image {
-			margin: 0% 20%;
-			@media screen and (max-width: $bp) {
-				margin: 0%;
-			}
-			img {
-				width: 60%;
-				@media screen and (max-width: $bp) {
-					width: 100%;
-				}
-			}
-		}
 		.avatar-container {
 			display: grid;
-			grid-template-columns: 1fr  1fr;
+			grid-template-columns: 1fr 1fr;
 			grid-gap: get-vw(60px);
 			@media screen and (min-width: 1240px) {
-				grid-gap: min(get-vw(60px),60px);
+				grid-gap: min(get-vw(60px), 60px);
 			}
 			@media screen and (max-width: $bp) {
 				grid-template-columns: 1fr;
-				grid-gap: max(get-vw(60px),60px);
+				grid-gap: max(get-vw(60px), 60px);
 			}
-
 		}
 		.avatar-cards {
 			width: get-vw(300px);
@@ -133,9 +120,19 @@
 				position: absolute;
 				top: 0%;
 				left: 0%;
-				background-image: linear-gradient(0deg, #11131C 0%, rgba(240, 173, 183, 0) 56.28%, rgba(17, 19, 28, 0) 56.28%);
+				background-image: linear-gradient(
+					0deg,
+					#11131c 0%,
+					rgba(240, 173, 183, 0) 56.28%,
+					rgba(17, 19, 28, 0) 56.28%
+				);
 				@media screen and (max-width: $bp) {
-					background-image: linear-gradient(0deg, #11131ca5 0%, rgba(240, 173, 183, 0) 25%, rgba(17, 19, 28, 0) 56.28%);
+					background-image: linear-gradient(
+						0deg,
+						#11131ca5 0%,
+						rgba(240, 173, 183, 0) 25%,
+						rgba(17, 19, 28, 0) 56.28%
+					);
 				}
 			}
 			img {
@@ -164,10 +161,10 @@
 				line-height: 105%;
 				@media screen and (min-width: 1240px) {
 					font-size: min(get-vw(37px), 37px);
-					max-width: min(get-vw(180px),180px);
+					max-width: min(get-vw(180px), 180px);
 				}
 				@media screen and (max-width: $bp) {
-					max-width: max(get-vw(180px),180px);
+					max-width: max(get-vw(180px), 180px);
 					font-size: max(get-vw(37px), 37px);
 				}
 			}
@@ -186,11 +183,15 @@
 
 		//end
 		.goodbye-message {
-			height: 100vh;
+			height: 120vh;
 			display: grid;
 			place-items: center;
+			h1 {
+				text-align: center;
+				line-height: 101%;
+			}
 			@media screen and (min-width: 1240px) {
-				height: min(100vh, 660px);
+				height: min(150vh, 875px);
 			}
 			@media screen and (max-width: $bp) {
 				height: max(100vh, 660px);
@@ -201,48 +202,47 @@
 	// fonts and misc styling
 	h1 {
 		margin-top: get-vw(40px);
-		margin-bottom: get-vw(40px);
+		margin-bottom: 1.5%;
 		font-size: get-vw(89px);
-		text-align: center;
+		// text-align: center;
 
 		@media screen and (min-width: 1240px) {
 			font-size: min(get-vw(89px), 89px);
 			margin-top: min(get-vw(40px), 40px);
-			margin-bottom: min(get-vw(40px), 40px);
 		}
 		@media screen and (max-width: $bp) {
 			text-align: left;
 			font-size: max(get-vw(89px), 59px);
 			margin-top: max(get-vw(40px), 40px);
-			margin-bottom: max(get-vw(40px), 40px);
 		}
 	}
 	h1.text {
 		//gradient
-		background: linear-gradient(96.56deg, #fff2f0 49.86%, #fad8d5 67.3%);
+		background: linear-gradient(96.56deg, #fff2f0 0%, #fcafa8 25%, #f5938b 40%);
 		-webkit-background-clip: text;
 		background-clip: text;
 		-webkit-text-fill-color: transparent;
 	}
 	h2 {
-		margin: get-vw(20px), 0%;
+		margin: 2% 0% 1% 0%;
 		font-size: get-vw(37px);
 		@media screen and (min-width: 1240px) {
 			font-size: min(get-vw(37px), 37px);
-			margin: min(get-vw(20px), 20px) 0%;
 		}
 		@media screen and (max-width: $bp) {
 			font-size: max(get-vw(37px), 37px);
-			margin: max(get-vw(20px), 20px) 0%;
 		}
 	}
 	p {
+		opacity: 0.85;
 		font-size: 20px;
 		font-style: normal;
 		font-weight: 400;
-		line-height: 35px;
+		line-height: 1.75;
 		letter-spacing: 0em;
 		text-align: left;
+		margin-top: 0.5%;
+		margin-bottom: 7%;
 	}
 
 	// spacers
@@ -259,9 +259,9 @@
 		}
 	}
 	.vert.two {
-		height: get-vw(55px);
+		height: get-vw(5px);
 		@media screen and (min-width: 1240px) {
-			height: min(get-vw(55px), 55px);
+			height: min(get-vw(5px), 5px);
 		}
 		@media screen and (max-width: $bp) {
 			height: 0px;
