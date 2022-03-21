893fbe9acb151c83aed56faa9b833d17120b733a
Ojasvin Kirpane <okirp@MacBook-Pro-2.local>
2022-03-21 21:49:42 +0530

    launchTest 1

---
+++ src/routes/launchTest.svelte
@@ -0,0 +1,32 @@
+<svelte:head>
+	<script>
+		if (!window.mootrack) {
+			!(function (t, n, e, o, a) {
+				function d(t) {
+					var n = ~~(Date.now() / 3e5),
+						o = document.createElement(e);
+					(o.async = !0), (o.src = t + '?ts=' + n);
+					var a = document.getElementsByTagName(e)[0];
+					a.parentNode.insertBefore(o, a);
+				}
+				(t.MooTrackerObject = a),
+					(t[a] =
+						t[a] ||
+						function () {
+							return t[a].q ? void t[a].q.push(arguments) : void (t[a].q = [arguments]);
+						}),
+					window.attachEvent
+						? window.attachEvent('onload', d.bind(this, o))
+						: window.addEventListener('load', d.bind(this, o), !1);
+			})(
+				window,
+				document,
+				'script',
+				'mootrack'
+			);
+		}
+		mootrack('loadForm', '7b8e8353ac044d15ba41558b6fa9d886');
+	</script>
+</svelte:head>
+
+<div data-mooform-id="7b8e8353-ac04-4d15-ba41-558b6fa9d886" />
