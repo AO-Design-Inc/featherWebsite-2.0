1ac9f951522b551a9580a77a6f71c192301dec8b
Ojasvin Kirpane <okirp@MacBook-Pro-2.local>
2022-03-21 21:59:55 +0530

    added ./ for routing

---
+++ .vercel_build_output/static/_app/pages/launchTest.svelte-a4f3bbf2.js
@@ -0,0 +1,41 @@
+import{S as R,i as Y,s as $,e as o,t as _,j as v,$ as z,c as s,a as i,g as b,d as n,l as h,b as l,E as t,f as P,F as C}from"../chunks/vendor-8aa06787.js";function A(O){let d,k,y,e,m,g,D,u,q,I,f,S,r,V,j,x,B,p,F,w;return{c(){d=o("script"),k=_(`if (!window.mootrack) {
+			!(function (t, n, e, o, a) {
+				function d(t) {
+					var n = ~~(Date.now() / 3e5),
+						o = document.createElement(e);
+					(o.async = !0), (o.src = t + '?ts=' + n);
+					var a = document.getElementsByTagName(e)[0];
+					a.parentNode.insertBefore(o, a);
+				}
+				(t.MooTrackerObject = a),
+					(t[a] =
+						t[a] ||
+						function () {
+							return t[a].q ? void t[a].q.push(arguments) : void (t[a].q = [arguments]);
+						}),
+					window.attachEvent
+						? window.attachEvent('onload', d.bind(this, o))
+						: window.addEventListener('load', d.bind(this, o), !1);
+			})(window, document, 'script', './src/routes/moosend-tracking.min.js', 'mootrack');
+		}
+		mootrack('loadForm', '7b8e8353ac044d15ba41558b6fa9d886');`),y=v(),e=o("div"),m=o("div"),g=_("You're invited to:"),D=v(),u=o("div"),q=_("The Future of Compute"),I=v(),f=o("div"),S=v(),r=o("div"),V=_("Tuesday, March 29th"),j=o("br"),x=_("2pm EST / 11am PST"),B=v(),p=o("div"),F=v(),w=o("div"),this.h()},l(c){const E=z('[data-svelte="svelte-1vokeid"]',document.head);d=s(E,"SCRIPT",{});var M=i(d);k=b(M,`if (!window.mootrack) {
+			!(function (t, n, e, o, a) {
+				function d(t) {
+					var n = ~~(Date.now() / 3e5),
+						o = document.createElement(e);
+					(o.async = !0), (o.src = t + '?ts=' + n);
+					var a = document.getElementsByTagName(e)[0];
+					a.parentNode.insertBefore(o, a);
+				}
+				(t.MooTrackerObject = a),
+					(t[a] =
+						t[a] ||
+						function () {
+							return t[a].q ? void t[a].q.push(arguments) : void (t[a].q = [arguments]);
+						}),
+					window.attachEvent
+						? window.attachEvent('onload', d.bind(this, o))
+						: window.addEventListener('load', d.bind(this, o), !1);
+			})(window, document, 'script', './src/routes/moosend-tracking.min.js', 'mootrack');
+		}
+		mootrack('loadForm', '7b8e8353ac044d15ba41558b6fa9d886');`),M.forEach(n),E.forEach(n),y=h(c),e=s(c,"DIV",{class:!0});var a=i(e);m=s(a,"DIV",{class:!0});var N=i(m);g=b(N,"You're invited to:"),N.forEach(n),D=h(a),u=s(a,"DIV",{class:!0});var L=i(u);q=b(L,"The Future of Compute"),L.forEach(n),I=h(a),f=s(a,"DIV",{class:!0}),i(f).forEach(n),S=h(a),r=s(a,"DIV",{class:!0});var T=i(r);V=b(T,"Tuesday, March 29th"),j=s(T,"BR",{}),x=b(T,"2pm EST / 11am PST"),T.forEach(n),B=h(a),p=s(a,"DIV",{class:!0}),i(p).forEach(n),F=h(a),w=s(a,"DIV",{"data-mooform-id":!0}),i(w).forEach(n),a.forEach(n),this.h()},h(){l(m,"class","hundred-width subheader svelte-1m8ndy6"),l(u,"class","hundred-width title svelte-1m8ndy6"),l(f,"class","spacer first two svelte-1m8ndy6"),l(r,"class","hundred-width date-time opacity-low svelte-1m8ndy6"),l(p,"class","spacer second svelte-1m8ndy6"),l(w,"data-mooform-id","7b8e8353-ac04-4d15-ba41-558b6fa9d886"),l(e,"class","title-container center center-text svelte-1m8ndy6")},m(c,E){t(document.head,d),t(d,k),P(c,y,E),P(c,e,E),t(e,m),t(m,g),t(e,D),t(e,u),t(u,q),t(e,I),t(e,f),t(e,S),t(e,r),t(r,V),t(r,j),t(r,x),t(e,B),t(e,p),t(e,F),t(e,w)},p:C,i:C,o:C,d(c){n(d),c&&n(y),c&&n(e)}}}class H extends R{constructor(d){super();Y(this,d,null,A,$,{})}}export{H as default};
--- src/routes/launchTest.svelte
+++ src/routes/launchTest.svelte
@@ -18,17 +18,58 @@
 					window.attachEvent
 						? window.attachEvent('onload', d.bind(this, o))
 						: window.addEventListener('load', d.bind(this, o), !1);
-			})(
-				window,
-				document,
-				'script',
-				'src/routes/moosend-tracking.min.js',
-				'mootrack'
-			);
+			})(window, document, 'script', './src/routes/moosend-tracking.min.js', 'mootrack');
 		}
 		mootrack('loadForm', '7b8e8353ac044d15ba41558b6fa9d886');
 	</script>
 </svelte:head>
 
-<div data-mooform-id="7b8e8353-ac04-4d15-ba41-558b6fa9d886" ></div>
+<div class="title-container center center-text">
+	<div class="hundred-width subheader">You're invited to:</div>
+	<div class="hundred-width title">The Future of Compute</div>
+	<div class="spacer first two" />
+	<div class="hundred-width date-time opacity-low">Tuesday, March 29th<br />2pm EST / 11am PST</div>
+	<div class="spacer second" />
+	<div data-mooform-id="7b8e8353-ac04-4d15-ba41-558b6fa9d886" />
+</div>
 
+<style lang="scss">
+	@import '../variables';
+	.spacer.first {
+		width: 100%;
+		height: get-vw(60px);
+		@media screen and (min-width: 1240px) {
+			height: min(get-vw(60px), 60px);
+		}
+		@media screen and (max-width: $bp) {
+			height: max(get-vw(60px), 60px);
+		}
+	}
+	.spacer.first.two {
+		height: get-vw(10px);
+		@media screen and (min-width: 1240px) {
+			height: min(get-vw(10px), 10px);
+		}
+		@media screen and (max-width: $bp) {
+			height: max(get-vw(10px), 10px);
+		}
+	}
+	.spacer.second {
+		width: 100%;
+		height: get-vw(45px);
+		@media screen and (min-width: 1240px) {
+			height: min(get-vw(45px), 45px);
+		}
+		@media screen and (max-width: $bp) {
+			height: max(get-vw(45px), 45px);
+		}
+	}
+	.title-container,
+	.hundred-width {
+		width: 100%;
+	}
+	.center {
+		display: grid;
+		place-items: center;
+	}
+</style>
